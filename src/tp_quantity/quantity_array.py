0250babddeb7d724e04d1c50e1aaa3aa63da9052
cage433 <cage433@gmail.com>
2025-12-08 09:18:02 +0800

    Add qty_array

---
+++ src/tp_quantity/quantity_array.py
@@ -0,0 +1,73 @@
+from numbers import Number
+
+import numpy as np
+from numpy import ndarray
+from tp_utils.type_utils import checked_type
+
+from tp_quantity.quantity import Qty
+from tp_quantity.uom import UOM, SCALAR
+
+
+class QtyArray:
+    def __init__(self, values, uom: UOM):
+        if isinstance(values, Number):
+            self.values: ndarray = np.asarray([values])
+        else:
+            self.values: ndarray = np.asarray(values)
+        self.uom: UOM = checked_type(uom, UOM)
+
+    def __str__(self):
+        low = self.values.min()
+        high = self.values.max()
+        mean = self.values.mean()
+        std = self.values.std()
+        text = f"[{low:1.2f}, {mean:1.2f} ({std:1.2f}), {high:1.2f}]"
+        if self.uom == SCALAR:
+            return text
+        return f"{text} {self.uom}"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def almost_equals(self, other, tol = 1e-6):
+        return (isinstance(other, QtyArray) and self.values.shape == other.values.shape and self.uom == other.uom
+                and abs(self.values - other.values).max() < tol)
+
+    @property
+    def shape(self):
+        return self.values.shape
+
+    def __mul__(self, other) -> 'QtyArray':
+        checked_type(other, (Qty, QtyArray))
+        if isinstance(other, Qty):
+            return QtyArray(self.values * other.value, self.uom * other.uom)
+        assert self.shape == other.shape, f"Mismatching shapes {self.shape} vs {other.shape}"
+        return QtyArray(self.values * other.values, self.uom * other.uom)
+
+
+    def __add__(self, other) -> 'QtyArray':
+        checked_type(other, (Qty, QtyArray))
+        if self.uom != other.uom:
+            raise ValueError(f"Cannot add Qtys with different uom, {self.uom} vs {other.uom}")
+        if isinstance(other, Qty):
+            return QtyArray(self.values + other.value, self.uom)
+        assert self.shape == other.shape, f"Mismatching shapes {self.shape} vs {other.shape}"
+        return QtyArray(self.values + other.values, self.uom)
+
+    def negate(self):
+        return QtyArray(self.values * -1, self.uom)
+
+    def __sub__(self, other) -> 'QtyArray':
+        checked_type(other, (Qty, QtyArray))
+        return self + other.negate()
+
+    def __truediv__(self, other) -> 'QtyArray':
+        checked_type(other, (Qty, QtyArray))
+        if isinstance(other, Qty):
+            return QtyArray(self.values / other.value, self.uom / other.uom)
+        assert self.shape == other.shape, f"Mismatching shapes {self.shape} vs {other.shape}"
+        return QtyArray(self.values / other.values, self.uom / other.uom)
+
+    @property
+    def inverse(self):
+        return QtyArray(1.0 / self.values, self.uom.inverse)
---
+++ tests/test_qty_array.py
@@ -0,0 +1,76 @@
+import unittest
+
+import numpy as np
+
+from tp_quantity.quantity import Qty
+from tp_quantity.quantity_array import QtyArray
+from tp_quantity.uom import USD, SCALAR, MWH, MT
+
+
+class QtyArrayTestCase(unittest.TestCase):
+
+    def test_str(self):
+        self.assertEqual(
+            '[1.00, 1.00 (0.00), 1.00] USD', str(QtyArray(1, USD))
+        )
+        self.assertEqual(
+            '[1.00, 2.00 (0.82), 3.00] USD',
+            str(QtyArray([1, 2, 3], USD)),
+        )
+        self.assertEqual(
+            '[1.00, 2.00 (0.82), 3.00]',
+            str(QtyArray([1, 2, 3], SCALAR)),
+        )
+
+    def _check_close(self, q1: QtyArray, q2: QtyArray):
+        self.assertTrue(q1.almost_equals(q2))
+
+    def test_construction(self):
+        self._check_close(
+            QtyArray(1, USD),
+            QtyArray([1], USD)
+        )
+        self._check_close(
+            QtyArray(1, USD),
+            QtyArray(np.asarray([1]), USD)
+        )
+        self.assertNotEqual(
+            QtyArray(1, USD),
+            QtyArray(1, MWH),
+        )
+
+    def test_multiplication(self):
+        self._check_close(
+            QtyArray(2, USD / MWH) * QtyArray(3, MWH),
+            QtyArray(6, USD)
+        )
+        self._check_close(
+            QtyArray(2, USD / MWH) * Qty(3, MWH),
+            QtyArray(6, USD)
+        )
+        self._check_close(
+            QtyArray(2, MWH.inverse) * QtyArray(3, MWH),
+            QtyArray(6, SCALAR)
+        )
+        self._check_close(
+            QtyArray([6, 12], MWH) / Qty(3, MWH),
+            QtyArray([2.0, 4.0], SCALAR)
+        )
+        self._check_close(
+            QtyArray([6, 12], MWH) / QtyArray([3, 6], MWH),
+            QtyArray([2.0, 2.0], SCALAR)
+        )
+
+    def test_addition(self):
+        self._check_close(
+            QtyArray(3, USD) + Qty(2, USD),
+            QtyArray(5, USD)
+        )
+        self._check_close(
+            QtyArray([3, 4], USD) + QtyArray([2, 1], USD),
+            QtyArray([5, 5], USD)
+        )
+        self._check_close(
+            QtyArray([3, 4], USD) - QtyArray([2, 1], USD),
+            QtyArray([1, 3], USD)
+        )
